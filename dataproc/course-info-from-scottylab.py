9292bdf3819621ed7524312403c1f72c41e610e5
Ting2004 <88258612+Ting2004@users.noreply.github.com>
2023-02-04 10:54:58 -0500

    semi-cleaned up

--- dataproc/course-info-from-scottylab.py
+++ dataproc/course-info-from-scottylab.py
@@ -3,9 +3,11 @@ import json
 import re
 import requests
 
+
 course_catalog_link = "https://enr-apps.as.cmu.edu/assets/SOC/sched_layout_spring.htm"
 r_coursecatalog = requests.get(course_catalog_link)
-FILE_NAME = "S23-full.txt"
+SOURCE_FILE_NAME = "S23-full.txt"
+TARGET_FILE_NAME = "full_list.json"
 sample_list = ["15-122", "15-112", "80-285", "21-127", "80-100"]
 course_information = {}
 
@@ -36,16 +38,15 @@ def get_course_code(text, dash = False):
                 course_list.append(obj.group())
     return course_list
 
-
+# it's too 'precise', don't touch
 def add_dash(s):
     return s[:2] + "-" + s[2:]
 
-
 # input list of corse codes (should work with/without dash)
 # also input data from scottylab api
-# export to FILE_NAME, overwrite if file exists
+# export to SOURCE_FILE_NAME, overwrite if file exists
 def write_data_to_file(course_list, data):
-    f = open(FILE_NAME, "w")
+    f = open(SOURCE_FILE_NAME, "w")
     #g = open("raw-data.txt", "w")
     keywords = {"prereqs_obj": "prereq", "coreqs_obj": "coreq"}
     for course_key in course_list:
@@ -68,16 +69,15 @@ def write_data_to_file(course_list, data):
                 course_info.append(keywords[keyword] + ":" + "None")
         f.write("\n".join(course_info))
     f.close()
-    #g.close()
 
-# read from FILE_NAME
+# read from SOURCE_FILE_NAME
 # construct a dictionary of courses ---> for json export
-# for each course, all features are initialized to None
-# update content by eval
+# for each course, all features are initialized to [] to avoid null pointer XD
+# update content by eval! note! very problem-prone
 # WARNING! may cause problem here ---> "if ~(content[0] == "[" and content[-1] == "]"):"
 def read_data_from_file():
     global course_information
-    f = open(FILE_NAME, "r")
+    f = open(SOURCE_FILE_NAME, "r")
     current_course = None
     for line in f.readlines():
         if line == '\n':
@@ -92,18 +92,18 @@ def read_data_from_file():
 
                 #initialize
                 course_information[current_course] = dict()
-                course_information[current_course]["name"] = None
-                course_information[current_course]["units"] = None
-                course_information[current_course]["desc"] = None
-                course_information[current_course]["antireq"] = None
-                course_information[current_course]["min-grade"] = None
-                #course_information[current_course]["crosslisted"] = None
-                course_information[current_course]["prereq"] = None
-                course_information[current_course]["coreq"] = None
+                course_information[current_course]["name"] = []
+                course_information[current_course]["units"] = []
+                course_information[current_course]["desc"] = []
+                course_information[current_course]["antireq"] = []
+                course_information[current_course]["min-grade"] = []
+                # course_information[current_course]["crosslisted"] = []
+                course_information[current_course]["prereq"] = []
+                course_information[current_course]["coreq"] = []
             else:
                 if ~(content[0] == "[" and content[-1] == "]"):
                     if content == "None":
-                        course_information[current_course][keyword] = None    
+                        course_information[current_course][keyword] = []    
                     else:
                         course_information[current_course][keyword] = content
                 else:
@@ -112,7 +112,6 @@ def read_data_from_file():
             print(line)
 
     return 42
-
 
 # optional: load data from scottylab api, may take a while
 # retrieve data from course catalog and scottylab, according to course_list
@@ -132,17 +131,17 @@ def load_data(sample_mode = False, rescrape = False, dash = False):
     # load into Python dictionary
     read_data_from_file()
 
-    
+# expoort to a json file
+# file name is TARGET_FILE_NAME
 def load_to_json():
     # export to json file
-    with open('full_list.json', 'w') as json_file:
+    with open(TARGET_FILE_NAME, 'w') as json_file:
         json.dump(course_information, json_file)
-
 
 # input course_list and premade dictionary
 # try to scrape cross listed from scottylab web-based course tool
 # requires the code has dash! in 15-122 format
-def add_additional_info(course_list, course_information):
+def add_additional_info(course_list):
     keyword = "crosslisted"
     for code in course_list:
         try:
@@ -155,7 +154,7 @@ def add_additional_info(course_list, course_information):
             #print(get_content(r.text, keyword))
             content = get_content(r.text, keyword)
             if content == []:
-                course_information[code][keyword] = None
+                course_information[code][keyword] = []
             else:
                 course_information[code][keyword] = get_content(r.text, keyword)
             print(code)
@@ -175,20 +174,34 @@ def get_content(text, keyword):
 
 def cleaning():
     for course in course_information:
-        if course_information[course]["prereq"] != None:
+        if course_information[course]["prereq"] != []:
             course_information[course]["prereq"] = eval(course_information[course]["prereq"])
+        if course_information[course]["coreq"] != []:
+            course_information[course]["coreq"] = eval(course_information[course]["coreq"])
             # print(course_information[course]["prereq"])
             
 
-
-test = '{"units":"12.0","crosslisted":["24-633","27-701"],"coreqs":[],"prereqString":"39601 or 27765 or 39602 or 24632 or 27503","prereqs":["39-601","27-765","39-602","24-632","27-503"],"desc":"Hands-on laboratory projects will teach students about all aspects of metals additive manufacturing (AM). Students will learn how to use SOLIDWORKS for part design, create and transfer design files to the AM machines, run the machines to build parts, perform post-processing operations, and characterize AM parts. Student will work in teams and complete three separate lab projects, each utilizing a different material system, part design, AM process/machine, post-processing steps and characterization methods. A major lab report and presentation will be required for each of the three lab projects. The course includes weekly lectures to complement the laboratory component. Priority for enrollment will be given to students who have declared the Additive Manufacturing Minor.","courseID":"39-603","department":"CIT Interdisciplinary","name":"Additive Manufacturing Laboratory:","schedules":[{"_id":"5fb97407f5a3ef0fd12716b1","courseID":"39-603","year":2020,"semester":"spring","lectures":[],"sections":[{"instructors":["Beuth, Jack","DeVincent Wolf, Sandra"],"_id":"6021cdbba46d4637fd7160d9","name":"A","times":[{"days":[1,3],"_id":"6021cdbba46d4637fd7160da","begin":"09:30AM","end":"11:20AM","building":"SH","room":"208"}],"location":"Pittsburgh, Pennsylvania"}]},{"_id":"5fba39e53259b617f1b6f377","courseID":"39-603","year":2021,"semester":"spring","lectures":[],"sections":[{"instructors":["Beuth, Jack","DeVincent Wolf, Sandra"],"_id":"6021e361329f1238c45b43e6","name":"A","times":[{"days":[1,3],"_id":"6021e361329f1238c45b43e7","begin":"09:10AM","end":"11:00AM","building":"TEP","room":"1102"}],"location":"Pittsburgh, Pennsylvania"}]},{"_id":"618f1b378bd73c0c4705b15a","courseID":"39-603","year":2022,"semester":"spring","lectures":[],"sections":[{"instructors":["Beuth, Jack"],"_id":"618f1b378bd73c0c4705b15b","name":"A","times":[{"days":[1,3],"_id":"618f1b378bd73c0c4705b15c","begin":"10:10AM","end":"12:00PM","building":"PH","room":"A19C"}],"location":"Pittsburgh, Pennsylvania"}],"__v":0},{"_id":"6358ac06e0c88b7b5f661683","courseID":"39-603","year":2023,"semester":"spring","lectures":[],"sections":[{"times":[{"days":[1,3],"begin":"10:00AM","end":"11:50AM","building":"WEH","room":"6403","_id":"6358ac06e0c88b7b5f661685"}],"name":"A","instructors":["Beuth, Jack"],"location":"Pittsburgh, Pennsylvania","_id":"6358ac06e0c88b7b5f661684"}],"__v":0}],"id":null}'
+def it_does_fixing():
+    
+    # add crosslisted to course-information
+    with open("full_list with string 9am.json", "r") as read_file:
+        print("Converting JSON encoded data into Python dictionary")
+        dict_with_cross = json.load(read_file)
+    for key in course_information:
+        content = dict_with_cross[key]["crosslisted"]
+        if content == None:
+            course_information[key]["courselisted"] = []
+        else:
+            course_information[key]["courselisted"] = content
+            
 def main():
-    load_data(rescrape = False)
-    #add_additional_info(get_course_code(r_coursecatalog.text, dash = True), course_information)
-    #get_content(test, "crosslisted")
+    load_data()
+    # add_additional_info(get_course_code(r_coursecatalog.text, dash = True))
+    # get_content(test, "crosslisted")
+    
     cleaning()
     load_to_json()
-    
+    print("finished")    
     return 42
 
 main()
